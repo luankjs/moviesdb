2d3490ab7cb63a139f6885579f69a0a8da87c375
Luan Santos <luan@tootz.com.br>
2022-12-28 13:53:24 -0300

    improvement: move layout from app fiel to each page file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import type { AppProps } from 'next/app'
 
-import Layout from '../domain/Layout'
-
 import '../styles/main.scss'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return(
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Component {...pageProps} />
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 
+import Layout from '../domain/Layout'
+
 const HomePage = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Movies DB</title>
         <meta name="description" content="The Unofficial Database of Movies" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <h1>Welcome to MoviesDB!</h1>
-    </>
+    </Layout>
   )
 }
 
