81df9e20ed3783302dd8fb242c865cc6a671e3eb
Luan Santos <luan@tootz.com.br>
2022-12-28 13:46:45 -0300

    chore: remove unused imports for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-const Home = () => {
+const HomePage = () => {
   return (
     <>
       <Head>
@@ -18,4 +13,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default HomePage
