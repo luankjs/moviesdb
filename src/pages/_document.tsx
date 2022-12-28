07f2c352a127c5a990dc6f2e841b559f0d2a6dc4
Luan Santos <luan@tootz.com.br>
2022-12-28 01:53:31 -0300

    chore: add fontawesome cdn

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ const Document = () =>  {
   return (
     <Html lang="en">
       <Head>
+        <script src="https://kit.fontawesome.com/f0f1fd88a3.js" crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
