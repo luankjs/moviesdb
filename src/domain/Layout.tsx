2559e82e8dfd4ba0cbe188b6a99d181529577f83
Luan Santos <luan@tootz.com.br>
2022-12-28 01:55:36 -0300

    feat: layout theme

---
+++ src/domain/Layout.tsx
@@ -0,0 +1,16 @@
+import React, { PropsWithChildren }  from 'react'
+import Navbar from '../components/Navbar'
+
+interface Props {
+}
+
+const Layout: React.FC<PropsWithChildren<Props>> = ({children}) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  )
+}
+
+export default Layout
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app'
 
+import Layout from '../domain/Layout'
+
 import '../styles/main.scss'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return(
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default App
