120ca1218bbb5c0d9bab180e77c0fdd59bad41d0
Luan Santos <luan@tootz.com.br>
2022-12-28 18:02:23 -0300

    improvement: move common header tags to _document file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,7 @@ const HomePage = () => {
   return (
     <Layout>
       <Head>
-        <title>Movies DB</title>
-        <meta name="description" content="The Unofficial Database of Movies" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Movies DB | Filmes</title>
       </Head>
       <h1>Welcome to MoviesDB!</h1>
     </Layout>
