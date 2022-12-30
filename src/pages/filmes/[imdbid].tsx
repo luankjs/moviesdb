497c25745b2a32fb1c4cca04232180f644dd2ad3
Luan Santos <luan@tootz.com.br>
2022-12-30 16:10:13 -0300

    improvement: movie page grid structure

--- src/pages/filmes/[imdbid].tsx
+++ src/pages/filmes/[imdbid].tsx
@@ -92,7 +92,7 @@ const MoviePage = () => {
         <>
           <div className="container">
             <div className="row justify-content-center align-items-center my-5">
-              <div className="col-md-4 col-lg-3 mb-5 mb-md-0">
+              <div className="col-11 col-md-5 col-lg-4 col-xl-4 mb-5 mb-md-0 px-5 px-md-0">
                 <div className="ratio ratio-2x3 position-relative rounded-3 overflow-hidden shadow-lg">
                   <Image
                     src={
@@ -106,7 +106,7 @@ const MoviePage = () => {
                   />
                 </div>
               </div>
-              <div className="col-md-8 col-lg-7">
+              <div className="col-11 col-md-7 col-lg-8 col-xl-7">
                 <div className="px-md-2 px-xl-4">
                   <h1 className="fw-semibold">{movie?.Title}</h1>
                   <p className="text-gray-600">{movie?.Plot}</p>
@@ -197,15 +197,15 @@ const MoviePage = () => {
           <div className="my-5">
             <div className="container">
               <div className="row justify-content-center">
-                <div className="col-10">
+                <div className="col-11 col-md-10">
                   <h2 className="fs-3 mb-3">Avalições</h2>
                 </div>
               </div>
             </div>
-            <div className="bg-cyan-800 pt-5 p-md-5">
+            <div className="bg-cyan-800 pt-5 pb-4 p-md-5">
               <div className="container">
                 <div className="row justify-content-center">
-                  <div className="col-10">
+                  <div className="col-11 col-md-10">
                     {isLoadingReviews && !movieReviews ? (
                       <div className="w-100 d-flex align-items-center justify-content-center">
                         <ReactLoading
