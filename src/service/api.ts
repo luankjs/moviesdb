dc9db2fdee62bfa08814cbf99bd5b390f4cf01c7
Luan Santos <luan@tootz.com.br>
2022-12-30 12:19:35 -0300

    improvement: add interceptor to send token to tstapi when exists

--- src/service/api.ts
+++ src/service/api.ts
@@ -4,6 +4,23 @@ const tstapi = axios.create({
   baseURL: 'https://tstapi.ffcloud.com.br',
 })
 
+tstapi.interceptors.request.use(
+  (config) => {
+    if (config.headers) {
+      const storagedCurrentUser = localStorage.getItem('currentUser')
+      if (storagedCurrentUser)
+        config.headers.Authorization = `Bearer ${
+          JSON.parse(storagedCurrentUser).token
+        }`
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
 const omdbApi = axios.create({
   baseURL: 'https://www.omdbapi.com',
 })
